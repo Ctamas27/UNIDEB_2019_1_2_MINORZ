b4e29be2462b543eaecd3f1c383a7ad7206b7f20
ctamas27 <ctamas27@gmail.com>
2019-12-01 20:32:31 +0100

    valami

--- hellofx/src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-module games {
-    requires javafx.fxml;
-    requires javafx.controls;
-    opens  sample;
-}
--- hellofx/src/main/java/org/openjfx/Main.java
+++
@@ -1,9 +0,0 @@
-package org.openjfx;
-
-public class Main {
-
-    public static void main(String[] args) {
-        App.main(args);
-    }
-
-}
