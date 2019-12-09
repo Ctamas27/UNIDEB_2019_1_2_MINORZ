a0209424c7b48c8417df79a233c47b31733be0cf
ctamas27 <ctamas27@gmail.com>
2019-12-09 12:16:57 +0100

    junit teszt

--- fourinarow/src/sample/Test.java
+++ fourinarow/src/sample/Test.java
@@ -1,7 +1,9 @@
 package sample;
 
+import javafx.scene.shape.Circle;
 import org.junit.Before;
 import  static org.junit.Assert.assertEquals;
+import static javafx.scene.paint.Color.BLUE;
 
 import java.awt.*;
 
@@ -9,10 +11,13 @@ public class Test {
 
     private Label testLabel = new Label();
 
+    private Circle testCircle = new Circle();
+
     @Before
     public void init(){
         testLabel.setText("valami");
         testLabel.setBackground(Color.red);
+        testCircle.setId("valami");
     }
 
     @org.junit.Test
@@ -24,5 +29,11 @@ public class Test {
     public void testLabelBackgroundColor(){
         assertEquals("Test for a label background color", Color.red, testLabel.getBackground());
     }
+
+    @org.junit.Test
+    public void testCircleId(){
+        assertEquals("Test for a color id", "valami", testCircle.getId());
+    }
+
 
 }
